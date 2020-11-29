636935bd3f8d5ad02ae948c7e451e9afdf87014c
zahar-create <zrogovskiy@gmail.com>
2020-11-29 23:21:54 +0600

    first task models

---
+++ tinder/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    video = models.FileField(upload_to="videos/")
+    description = models.TextField(max_length=500)
+    pitch_desk = models.FileField()
+
+
+class Users(AbstractUser):
+    investor = models.BooleanField(default=False)
+    company_type = models.TextField(max_length=15)
+    no_list = models.ManyToManyField(Project, related_name="no_list")
+    yes_list = models.ManyToManyField(Project, related_name="yes_list")
+
+
+class Image():
+    images = models.FileField()
